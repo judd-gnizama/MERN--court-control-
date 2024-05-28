4d35d878f9350f6c57d6e809727344d134c6e76e
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-05-28 14:18:25 +0800

    Initial Setup for Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <div className="App">Hello World</div>
+      <span class="material-symbols-outlined">home</span>
+    </>
+  );
+}
+
+export default App;
