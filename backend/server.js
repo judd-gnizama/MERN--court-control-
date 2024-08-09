5189b1e93842540d540a1bd4df813aedc918f8d1
judd.gnizama <juddmaverick@gmail.com>
2024-08-09 18:40:51 +0800

    render client

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use("/api/users", usersRoutes);
 
 // Render client
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+  res.sendFile(path.join(__dirname, "../frontend/public/index.html"))
 );
 
 mongoose
